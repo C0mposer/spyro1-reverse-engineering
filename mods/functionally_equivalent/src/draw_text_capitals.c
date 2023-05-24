6cbff0665c7253b6583ff1a9302298798e68643a
BuildTools <torbotax1@gmail.com>
2023-05-24 00:50:26 -0700

    DrawTextCapitals

--- mods/functionally_equivalent/src/draw_text_capitals.c
+++ mods/functionally_equivalent/src/draw_text_capitals.c
@@ -4,7 +4,7 @@
 //**********************************
 // ** Function: DrawTextCaoutals
 // ** Original Address: 0x80017fe4
-// ** Hook File: draw_text_capitals.s 
+// ** Hook File: draw_text_capitals_hook.s 
 // ** Prototype: n/a
 //**********************************
 /*
@@ -18,52 +18,51 @@
 char* DrawTextCapitals(char *text,int *textInfo, int spacing, char color)
 
 {
-  char currentCharacter;
-  short uVar2;
-  unsigned int currentCharacterInt;
-  
-  currentCharacter = *text;
-  while (currentCharacter != 0) {                                              //Not an end character
-    if (currentCharacter != 0x20) {                                            //Not a space
-      _ptr_HudMobyData = _ptr_HudMobyData - MOBY_SIZE;                         //Shifts the moby pointer to a new empty slot
-      memset(_ptr_HudMobyData, 0, MOBY_SIZE);                                  //Clears the new slot
-      Vec3Copy((int *)(_ptr_HudMobyData + 0xc),textInfo);                      //Copy text x pos, y pos, and size to the new moby
-      currentCharacter = *text;                                         //Puts the first character into a new variable
-      currentCharacterInt = (unsigned int)currentCharacter;
-      if (currentCharacterInt - '0' < 10) {                                //If the character is 0-9
-        *(unsigned short *)(_ptr_HudMobyData + 0x36) = currentCharacter + 0xd4;         //
+  unsigned int currentCharacter;                                                //? Needs to be unsigned lmao?
+
+  currentCharacter = *text;     
+                                                                                //Puts first character of string in currentcCharacter
+  while (currentCharacter != 0) {                                               //Not a NULL terminator
+    if (currentCharacter != 0x20) {                                             //Not a space character
+      _ptr_HudMobyData = _ptr_HudMobyData - MOBY_SIZE;                          //Shifts the moby pointer to a new empty slot
+      memset(_ptr_HudMobyData, 0, MOBY_SIZE);                                   //Clears the new slot
+      Vec3Copy((int *)(_ptr_HudMobyData + 0xc),textInfo);                       //Copy text x pos, y pos, and size(z pos) to the new moby
+      currentCharacter = *text;                                                 //Puts each character of the string in currentCharacter each iteration of the loop
+      if(currentCharacter - '0' < 10) {                                         //If currentCharacter 0-9
+        *(unsigned short *)(_ptr_HudMobyData + 0x36) = currentCharacter + 0xd4;         
       }
-      else if (currentCharacterInt - 'A' < 26) {                           //If the character is A-Z
-        *(unsigned short *)(_ptr_HudMobyData + 0x36) = currentCharacter + 0x169;        //
+      else if(currentCharacter - 'A' < 26) {                                    //If currentCharacter is A-Z
+        *(unsigned short *)(_ptr_HudMobyData + 0x36) = currentCharacter + 0x169;        
       }
-      else {
-        if (currentCharacterInt == '/') {
-          uVar2 = 0x115;
+      else {          
+        short mobyType;                                                          //Special Characters
+        if(currentCharacter == '/') {
+          mobyType = 0x115;
         }
-        else if (currentCharacterInt == '?') {
-          uVar2 = 0x116;
+        else if(currentCharacter == '?') {
+          mobyType = 0x116;
         }
-        else if (currentCharacterInt == '%') {
-          uVar2 = 0x110;
+        else if(currentCharacter == '%') {
+          mobyType = 0x110;
         }
-        else if (currentCharacterInt == '^') {
-          uVar2 = 0x141;
+        else if(currentCharacter == '^') {
+          mobyType = 0x141;
         }
-        else {
-          uVar2 = 0x147;
-          if (currentCharacterInt == '+') {
-            uVar2 = 0x13d;
+        else if(currentCharacter == '+') {
+          mobyType = 0x13d;
           }
+        else {                                                                  //Default Character (_)
+          mobyType = 0x147;
         }
-        *(short *)(_ptr_HudMobyData + 0x36) = uVar2;
+        *(short *)(_ptr_HudMobyData + 0x36) = mobyType;
       }
       _ptr_HudMobyData[0x47] = 0x7f;
       _ptr_HudMobyData[0x4f] = color;
       _ptr_HudMobyData[0x50] = 0xff;
     }
-    text = ((char *)text + 1);                                  //text string pointer is moved to the next character
-    *textInfo = *textInfo + spacing;                                    //The x position in the textInfo struct is increased by the spacing amount
-    currentCharacter = *text;                                           //currentCharacter is updated to the next character
+    text++;                                                                     //Move to next char in String
+    textInfo[0] += spacing;                                                     //The x position in the textInfo struct is increased by the spacing amount
+    currentCharacter = *text;                                                   //currentCharacter is updated to the next character
   }
   //printf("my print: %X\n", _ptr_HudMobyData);
   return _ptr_HudMobyData;
