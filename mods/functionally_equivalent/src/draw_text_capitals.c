dabd7b21bb5c8c39ff3c62ca4b1023bece57ec76
BuildTools <torbotax1@gmail.com>
2023-05-24 01:48:41 -0700

    DrawTextAll

--- mods/functionally_equivalent/src/draw_text_capitals.c
+++ mods/functionally_equivalent/src/draw_text_capitals.c
@@ -67,3 +67,79 @@ char* DrawTextCapitals(char *text,int *textInfo, int spacing, char color)
   //printf("my print: %X\n", _ptr_hudMobyData);
   return _ptr_hudMobyData;
 }
+
+
+int DrawTextAll(char *text,int *CapitalTextInfo,int *LowercaseTextInfo,int spacing,char color)
+
+{
+  char bVar1;
+  bool bVar2;
+  unsigned char *puVar3;
+  int iVar4;
+  unsigned int uVar5;
+  
+  bVar2 = TRUE;
+  bVar1 = *text;
+  while (bVar1 != 0) {
+    if (bVar1 == 0x20) {
+      iVar4 = *LowercaseTextInfo * 3;
+      bVar2 = TRUE;
+      if (iVar4 < 0) {
+        iVar4 = iVar4 + 3;
+      }
+      *CapitalTextInfo = *CapitalTextInfo + (iVar4 >> 2);
+    }
+    else {
+      _ptr_hudMobyData = _ptr_hudMobyData - MOBY_SIZE;
+      memset(_ptr_hudMobyData, '\0', MOBY_SIZE);
+      Vec3Copy((int *)(_ptr_hudMobyData + 0xc),CapitalTextInfo);
+      puVar3 = _ptr_hudMobyData;
+      if ((*text == 0x21) || (*text == 0x3f)) {
+        bVar2 = TRUE;
+      }
+      if (!bVar2) {
+        *(int *)(_ptr_hudMobyData + 0x10) = *(int *)(_ptr_hudMobyData + 0x10) + LowercaseTextInfo[1];
+        *(int *)(puVar3 + 0x14) = LowercaseTextInfo[2];
+      }
+      puVar3 = _ptr_hudMobyData;
+      bVar1 = *text;
+      uVar5 = (uint)bVar1;
+      if (uVar5 - 0x30 < 10) {
+        *(short *)(_ptr_hudMobyData + 0x36) = bVar1 + 0xd4;
+      }
+      else if (uVar5 - 0x41 < 0x1a) {
+        *(short *)(_ptr_hudMobyData + 0x36) = bVar1 + 0x169;
+      }
+      else if (uVar5 == 0x21) {
+        *(short *)(_ptr_hudMobyData + 0x36) = 0x4b;
+      }
+      else if (uVar5 == 0x2c) {
+        *(short *)(_ptr_hudMobyData + 0x36) = 0x4c;
+      }
+      else if (uVar5 == 0x3f) {
+        *(short *)(_ptr_hudMobyData + 0x36) = 0x116;
+      }
+      else if (uVar5 == 0x2e) {
+        *(short *)(_ptr_hudMobyData + 0x36) = 0x147;
+      }
+      else {
+        *(short *)(_ptr_hudMobyData + 0x36) = 0x4c;
+        *(int *)(puVar3 + 0x10) = *(int *)(puVar3 + 0x10) - (*LowercaseTextInfo << 1) / 3;
+      }
+      _ptr_hudMobyData[0x47] = '\x7f';
+      _ptr_hudMobyData[0x4f] = color;
+      _ptr_hudMobyData[0x50] = 0xff;
+      if (bVar2) {
+        iVar4 = *CapitalTextInfo + spacing;
+      }
+      else {
+        iVar4 = *CapitalTextInfo + *LowercaseTextInfo;
+      }
+      *CapitalTextInfo = iVar4;
+      bVar2 = *text - 0x30 < 10;
+    }
+    text = text + 1;
+    bVar1 = *text;
+  }
+  return _ptr_hudMobyData;
+}
