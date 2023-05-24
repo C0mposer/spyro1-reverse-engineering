dd8622e0b7453c4e8511f66673874853cbb061fa
BuildTools <torbotax1@gmail.com>
2023-05-24 01:19:21 -0700

    text.h

---
+++ include/text.h
@@ -0,0 +1,23 @@
+#ifndef TEXT_H
+#define TEXT_H
+
+//~~~~~~~
+//Structs
+//~~~~~~~
+struct CapitalTextInfo
+{
+    int x;                                  
+    int y;                                  
+    int size;                                        //Higher number, smaller size
+
+};
+
+
+struct LowercaseTextInfo
+{
+    int spacing;
+    int yOffset;
+    int size;                                        //Higher number, smaller size
+};
+
+#endif /* TEXT_H */
