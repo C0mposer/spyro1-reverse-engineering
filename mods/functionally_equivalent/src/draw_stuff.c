78f01276be7b7bbcdbe151bf6b80e6e36acae59b
BuildTools <torbotax1@gmail.com>
2023-08-01 00:35:39 -0700

    Draw Arrow

--- mods/functionally_equivalent/src/draw_stuff.c
+++ mods/functionally_equivalent/src/draw_stuff.c
@@ -17,23 +17,23 @@
 void DrawArrow(int *hudMobyInfo, uint timer, int leftOrRightArrow)
 
 {
-  if ((timer % 32) < 16) 
+  if ((timer % 32) < 16)                                    // Alternate from being visible, and not every 16 frames
   {  
-    _ptr_hudMobys = _ptr_hudMobys + -0x58;
-    memset(_ptr_hudMobys,'\0',0x58);
+    _ptr_hudMobys = _ptr_hudMobys + -0x58;                  // Make space in the array of moby structs to be drawn to the hud
+    memset(_ptr_hudMobys,'\0',0x58);                        // 0 out the area
 
     *(short *)(_ptr_hudMobys + 0x36) = 0x105;               // Set Moby Type to the ascii number 1 for an arrow
 
-    Vec3Copy((_ptr_hudMobys + 0xc), hudMobyInfo);         // Set Moby Position from info struct (Z represents size/depth in 2D)
+    Vec3Copy((_ptr_hudMobys + 0xc), hudMobyInfo);           // Set Moby Position from info struct (Z represents size/depth in 2D)
 
     if (leftOrRightArrow < 2) {
-      _ptr_hudMobys[0x44] = 64;                             // Setting default roll rotation?
+      _ptr_hudMobys[0x44] = 64;                             // Setting default roll rotation for a right arrow
       _ptr_hudMobys[0x46] = (char)(leftOrRightArrow * 128); // Bitshifting the arrow choice by 7 (multiplying by 128) to result in
-                                                            // the yaw rotation of the number 1 to on the oppisite side if.
+                                                            // the yaw rotation of the number 1 to a left arrow.
     }
-    _ptr_hudMobys[0x47] = '\x7f';
-    _ptr_hudMobys[0x4f] = '\v';
-    _ptr_hudMobys[0x50] = 0xff;
+    _ptr_hudMobys[0x47] = 0x7f;                             // Required for hud? Must be positive
+    _ptr_hudMobys[0x4f] = 11;                               // Setting the color to gold
+    _ptr_hudMobys[0x50] = -1;                               // Required for hud 2? Must be negative
   }
   return;
 }
