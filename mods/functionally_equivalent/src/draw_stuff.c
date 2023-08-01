036002678227fa6237757424a415e28323d8542b
BuildTools <torbotax1@gmail.com>
2023-08-01 00:59:28 -0700

    Draw

--- mods/functionally_equivalent/src/draw_stuff.c
+++ mods/functionally_equivalent/src/draw_stuff.c
@@ -1,5 +1,6 @@
 #include <common.h>
 #include <custom_types.h>
+#include <moby.h>
 
 //**********************************
 // ** Function: DrawArrow
@@ -14,26 +15,26 @@
  * @param unsigned int timer - timer that counts every frame to be used for the pre-determined interval.
  * @param int leftOrRightArrow - 0 for right, 1 for left
 */
-void DrawArrow(int *hudMobyInfo, uint timer, int leftOrRightArrow)
+void DrawArrow(int* hudMobyInfo, uint timer, int leftOrRightArrow)
 
 {
-  if ((timer % 32) < 16)                                    // Alternate from being visible, and not every 16 frames
+  if ((timer % 32) < 16)                                                // Alternate from being visible, and not every 16 frames
   {  
-    _ptr_hudMobys = _ptr_hudMobys + -0x58;                  // Make space in the array of moby structs to be drawn to the hud
-    memset(_ptr_hudMobys,'\0',0x58);                        // 0 out the area
+    _ptr_hudMobys = _ptr_hudMobys - MOBY_SIZE;                          // Make space in the array of moby structs to be drawn to the hud
+    memset(_ptr_hudMobys,'\0', MOBY_SIZE);                              // 0 out the area
 
-    *(short *)(_ptr_hudMobys + 0x36) = 0x105;               // Set Moby Type to the ascii number 1 for an arrow
+    ((Moby*) _ptr_hudMobys)->type = 0x105;                              // Set Moby Type to the ascii number 1 for an arrow
 
-    Vec3Copy((_ptr_hudMobys + 0xc), hudMobyInfo);           // Set Moby Position from info struct (Z represents size/depth in 2D)
+    Vec3Copy(&((Moby*) _ptr_hudMobys)->position, hudMobyInfo);          // Set Moby Position from info struct (Z represents size/depth in 2D)
 
     if (leftOrRightArrow < 2) {
-      _ptr_hudMobys[0x44] = 64;                             // Setting default roll rotation for a right arrow
-      _ptr_hudMobys[0x46] = (char)(leftOrRightArrow * 128); // Bitshifting the arrow choice by 7 (multiplying by 128) to result in
-                                                            // the yaw rotation of the number 1 to a left arrow.
+        ((Moby*) _ptr_hudMobys)->rotation.x = 64;                               // Setting default roll rotation for a right arrow
+        ((Moby*) _ptr_hudMobys)->rotation.z = (byte)(leftOrRightArrow * 128);   // Bitshifting the arrow choice by 7 (multiplying by 128) to result in
+                                                                                // the yaw rotation of the number 1 to a left arrow.
     }
-    _ptr_hudMobys[0x47] = 0x7f;                             // Required for hud? Must be positive
-    _ptr_hudMobys[0x4f] = 11;                               // Setting the color to gold
-    _ptr_hudMobys[0x50] = -1;                               // Required for hud 2? Must be negative
+    ((Moby*) _ptr_hudMobys)->requiredHUD1 = 0x7f;           // Required for hud? Must be positive
+    ((Moby*) _ptr_hudMobys)->color = MOBY_COLOR_GOLD;       // Setting the color to gold
+    ((Moby*) _ptr_hudMobys)->requiredHUD2 = -1;             // Required for hud 2? Must be negative
   }
   return;
 }
